7dc65e0e83b3ba2005a8960d1da4711e1f536743
Sathyanarayanan Gunasekaran <gsathya.ceg@gmail.com>
2014-08-29 22:54:36 -0400

    add ability to send and list experiments

--- config.py
+++ config.py
@@ -1,1 +1,8 @@
+import os
+import getpass
+
 recommended_versions = ["1.1"]
+current_user = getpass.getuser()
+centinel_home = os.path.join(os.path.expanduser('~'+current_user), '.centinel')
+results_dir = os.path.join(centinel_home, 'results')
+experiments_dir = os.path.join(centinel_home, 'experiments')
--- server.py
+++ server.py
@@ -1,5 +1,7 @@
 import config
+import glob
 import flask
+import os
 
 app = flask.Flask(__name__)
 
@@ -7,10 +9,31 @@ app = flask.Flask(__name__)
 def get_recommended_versions():
     return flask.jsonify({"versions" : config.recommended_versions})
 
-@app.route("/results", method=['POST']):
+@app.route("/results", methods=['GET', 'POST'])
 def submit_result():
-    if request.method == "POST":
+    if flask.request.method == "POST":
         pass
+    else:
+        # look in results directory
+        results = []
+        for path in glob.glob(os.path.join(config.results_dir,'[!_]*.json')):
+            results.append(path)
+        return flask.jsonify({"result_files":results})
+
+@app.route("/experiments/")
+@app.route("/experiments/<name>")
+def get_experiment_list(name=None):
+    experiments = {}
+
+    for path in glob.glob(os.path.join(config.experiments_dir,'[!_]*.py')):
+        # get name of file and path
+        file_name, ext = os.path.splitext(os.path.basename(path))
+        experiments[file_name] = path
+
+    if name in experiments:
+        return "Experiment found"
+    else:
+        return flask.jsonify({"experiments" : experiments.keys()})
 
 if __name__ == "__main__":
     app.run(debug=True)
