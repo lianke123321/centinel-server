828c78566879412c6e2cc6981af9fa1adb5bdcf4
Sathyanarayanan Gunasekaran <gsathya.ceg@gmail.com>
2014-08-30 10:40:54 -0400

    return result files, not just names

--- server.py
+++ server.py
@@ -16,25 +16,31 @@ def submit_result():
         pass
     else:
         # look in results directory
-        results = []
+        results = {}
         for path in glob.glob(os.path.join(config.results_dir,'[!_]*.json')):
-            results.append(path)
-        return flask.jsonify({"result_files":results})
+            # get name of file and path
+            file_name, ext = os.path.splitext(os.path.basename(path))
+            with open(path) as result_file:
+                results[file_name] = json.load(result_file)
+        return flask.jsonify({"results":results})
 
 @app.route("/experiments/")
 @app.route("/experiments/<name>")
 def get_experiment_list(name=None):
     experiments = {}
-
+    # look for experiments in experiments directory
     for path in glob.glob(os.path.join(config.experiments_dir,'[!_]*.py')):
         # get name of file and path
         file_name, ext = os.path.splitext(os.path.basename(path))
         experiments[file_name] = path
 
+    if name == None:
+        return flask.jsonify({"experiments" : experiments.keys()})
+
     if name in experiments:
         return "Experiment found"
     else:
-        return flask.jsonify({"experiments" : experiments.keys()})
+        return "Experiment not found"
 
 @app.route("/clients/")
 @app.route("/clients/<name>")
@@ -43,11 +49,13 @@ def get_clients(name=None):
     with open(config.clients_file) as clients_fh:
         clients = json.load(clients_fh)
 
-    if name not in clients:
+    if name == None:
         return flask.jsonify(clients)
-    else:
+
+    if name in clients:
         return flask.jsonify(client[name])
-        
-    
+    else:
+        return "Client not found"
+
 if __name__ == "__main__":
     app.run(debug=True)
