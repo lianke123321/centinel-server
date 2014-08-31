a35b8daf017c86a6829bdfcd7d06f55ab114db8e
Sathyanarayanan Gunasekaran <gsathya.ceg@gmail.com>
2014-08-30 23:48:18 -0400

    Cleanup config

--- config.py
+++ config.py
@@ -1,9 +1,14 @@
 import os
 import getpass
 
+# misc
 recommended_version = 1.1
-current_user = getpass.getuser()
-centinel_home = os.path.join(os.path.expanduser('~'+current_user), '.centinel')
-results_dir = os.path.join(centinel_home, 'results')
-experiments_dir = os.path.join(centinel_home, 'experiments')
-clients_file = os.path.join(centinel_home, 'clients')
+
+# user details
+current_user        = getpass.getuser()
+centinel_home       = os.path.join(os.path.expanduser('~'+current_user), '.centinel')
+
+# directory structure
+results_dir         = os.path.join(centinel_home, 'results')
+clients_file        = os.path.join(centinel_home, 'clients')
+experiments_dir     = os.path.join(centinel_home, 'experiments')
