7eb40dbc01440ca4d7543d2ab725dec2aca689e9
om.bhallamudi <om.bhallamudi@gmail.com>
2014-09-11 03:14:44 +0530

    fixed tab/space problem

--- tests.py
+++ tests.py
@@ -13,24 +13,24 @@ import base64
 import io
 
 class MyTest(TestCase):
-	
+
     testUsername = str(uuid.uuid4())
     testPassword = 'testingpassword'
 
     def create_app(self):
-		self.app = Flask(__name__)
-		self.app.config['TESTING'] = True
-		return app
+        self.app = Flask(__name__)
+        self.app.config['TESTING'] = True
+        return app
 
     def setUp(self):
-    	db.create_all()
-    	user = Client(username=self.testUsername,password=self.testPassword)
-    	db.session.add(user)
-    	db.session.commit()
+        db.create_all()
+        user = Client(username=self.testUsername,password=self.testPassword)
+        db.session.add(user)
+        db.session.commit()
 
     def tearDown(self):
-    	db.session.remove()
-    	db.drop_all()
+        db.session.remove()
+        db.drop_all()
 
     def open_with_auth(self, url, method, username, password):
         return self.client.open(url,
@@ -47,14 +47,14 @@ class MyTest(TestCase):
         self.assertTrue('Basic' in response.headers['WWW-Authenticate'])
 
     def test_version(self):
-    	url = '/version'
-    	response = self.client.get(url)
-    	self.assert_200(response)
+        url = '/version'
+        response = self.client.get(url)
+        self.assert_200(response)
         self.assertEquals(response.json, {"version" : config.recommended_version})
 
     def test_results_GET(self):
-    	url = '/results'	
-    	#Check for broken auth ->
+        url = '/results'
+        #Check for broken auth ->
         self.check_broken_auth(url)
         #Check working auth ->
         response = self.open_with_auth(url,'GET',self.testUsername, self.testPassword)
@@ -77,18 +77,18 @@ class MyTest(TestCase):
         os.remove('testfile')
 
     def test_experiments(self):
-    	url = '/experiments'
-    	response = self.client.get(url)
-    	self.assert_200(response)
-    	#XXX: Could be expanded?
-    	for experiment in response.json["experiments"]:
-    		url = '/experiments/{0}'.format(experiment)
-    		response_ = self.client.get(url)
-    		self.assert_200(response)
+        url = '/experiments'
+        response = self.client.get(url)
+        self.assert_200(response)
+        #XXX: Could be expanded?
+        for experiment in response.json["experiments"]:
+            url = '/experiments/{0}'.format(experiment)
+            response_ = self.client.get(url)
+            self.assert_200(response)
 
     def test_clients(self):
-    	url = '/clients'
-    	#Check for broken auth ->
+        url = '/clients'
+        #Check for broken auth ->
         self.check_broken_auth(url)
         #Check working auth ->
         response = self.open_with_auth(url,'GET',self.testUsername, self.testPassword)
@@ -96,19 +96,19 @@ class MyTest(TestCase):
         self.assertEquals(response.json, {"clients" : [self.testUsername]})
 
     def test_log(self):
-    	#XXX: TODO
-    	pass
+        #XXX: TODO
+        pass
     
     def test_register(self):
-    	url = '/register'
-    	testUsername = str(uuid.uuid4())
-    	testPassword = 'somepassword'
-    	response = self.client.post(url, \
-    		data = flask.json.dumps({'username': testUsername, 'password': testPassword}),\
-    	    content_type='application/json')
+        url = '/register'
+        testUsername = str(uuid.uuid4())
+        testPassword = 'somepassword'
+        response = self.client.post(url, \
+            data = flask.json.dumps({'username': testUsername, 'password': testPassword}),\
+            content_type='application/json')
         self.assertEquals(response.json, {"status" : "success"})
         self.assert_status(response,201)
 
 
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
