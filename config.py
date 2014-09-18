d56182ad2cb05324edfcaaddb3cf6f008947071d
Ben Jones <benjam2@g.clemson.edu>
2014-09-18 15:34:55 -0400

    added config entry for maxmind db location

--- config.py
+++ config.py
@@ -5,12 +5,14 @@ import getpass
 recommended_version = 1.1
 
 # user details
-current_user        = getpass.getuser()
-centinel_home       = os.path.join(os.path.expanduser('~'+current_user), '.centinel')
+current_user    = getpass.getuser()
+centinel_home   = os.path.join(os.path.expanduser('~'+current_user),
+                               '.centinel')
 
 # directory structure
-results_dir         = os.path.join(centinel_home, 'results')
-experiments_dir     = os.path.join(centinel_home, 'experiments')
+results_dir     = os.path.join(centinel_home, 'results')
+experiments_dir = os.path.join(centinel_home, 'experiments')
 
 # sqlite
-sqlite_db = os.path.join(centinel_home, 'db.sqlite')
+sqlite_db       = os.path.join(centinel_home, 'db.sqlite')
+maxmind_db      = os.path.join(centinel_home, 'maxmind.mmdb')
