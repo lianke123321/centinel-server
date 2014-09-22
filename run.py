a65cf4a2fe8272e144ed5025a501fb5d7b9dfcb7
Ben Jones <benjam2@g.clemson.edu>
2014-09-22 13:30:44 -0400

    updated for code review
    updated run.py to have better imports, put all of the functionality
    into a __name__ == main section, and reduced the levels in our code
    to create the db file/directory

--- run.py
+++ run.py
@@ -1,15 +1,21 @@
-from app import app, db, models
+import centinel
+import centinel.models
+import centinel.views
 import config
 import os
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///%s' % (config.sqlite_db)
-if not os.path.exists(config.sqlite_db):
+
+if __name__ == "__main__":
+    db = centinel.db
+    app = centinel.app
+
     sql_dir = os.path.dirname(config.sqlite_db)
     if not os.path.exists(sql_dir):
         os.makedirs(sql_dir)
-    db.create_all()
-    # create an admin and client role
-    db.session.add(models.Role('admin'))
-    db.session.add(models.Role('client'))
-    db.session.commit()
-app.run(debug=True)
+    if not os.path.exists(config.sqlite_db):
+        db.create_all()
+        # create an admin and client role
+        db.session.add(centinel.models.Role('admin'))
+        db.session.add(centinel.models.Role('client'))
+        db.session.commit()
+    app.run(debug=True)
