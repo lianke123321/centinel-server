79610f1b687d32e069c6b59e96b70fbab79e5d74
Abbas Razaghpanah <arazaghpanah@cs.stonybrook.edu>
2014-10-21 17:31:21 -0400

    added adhoc option (close #33), TLSv1 by default (close #30)

--- run.py
+++ run.py
@@ -1,11 +1,19 @@
+import argparse
 import centinel
 import centinel.models
 import centinel.views
 import config
+from OpenSSL import SSL
 import os
 
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--adhoc', help='Use adhoc SSL key and certificate.',
+                        action='store_true')
+    return parser.parse_args()
 
 if __name__ == "__main__":
+    args = parse_args()
     db = centinel.db
     app = centinel.app
 
@@ -18,8 +26,15 @@ if __name__ == "__main__":
         db.session.add(centinel.models.Role('admin'))
         db.session.add(centinel.models.Role('client'))
         db.session.commit()
+    if args.adhoc:
+        context='adhoc'
+    else:
+        # default method should be TLS
+        context = SSL.Context(SSL.TLSv1_METHOD)
+        context.use_privatekey_file(config.ssl_key)
+        context.use_certificate_file(config.ssl_cert)
 
     # Also, I shouldn't have to say this, but *DO NOT COMMIT THE
     # KEY*. Under no circumstances should the key be committed
     app.run(host="0.0.0.0", port=8082, debug=True,
-            ssl_context=(config.ssl_cert, config.ssl_key))
+            ssl_context=context)
