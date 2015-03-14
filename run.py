b194146e1c1fb9de378279033d2077acecbaeab2
Abbas Razaghpanah <arazaghpanah@cs.stonybrook.edu>
2015-03-14 19:38:35 -0400

    added AS number and AS info to /clients

--- run.py
+++ run.py
@@ -15,7 +15,7 @@ def parse_args():
 if __name__ == "__main__":
     args = parse_args()
     db = centinel.db
-    app = centinel.app   
+    app = centinel.app
     db.create_all()
 
     Role = centinel.models.Role
